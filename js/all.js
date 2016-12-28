e7b23f1eb579cc3ef1307c7036ed7e8d4a49666a
shortcutme <hello@noloop.me>
2016-12-28 13:54:47 +0100

    Add future post filtering to followed users

--- js/all.js
+++ js/all.js
@@ -4898,7 +4898,7 @@ function clone(obj) {
       if (this.directories === "all") {
         where = "WHERE post_id IS NOT NULL AND post.date_added < " + (Time.timestamp() + 120) + " ";
       } else {
-        where = "WHERE directory IN " + (Text.sqlIn(this.directories)) + " AND post_id IS NOT NULL ";
+        where = "WHERE directory IN " + (Text.sqlIn(this.directories)) + " AND post_id IS NOT NULL AND post.date_added < " + (Time.timestamp() + 120) + " ";
       }
       if (this.filter_post_id) {
         where += "AND post_id = :post_id ";
@@ -5030,6 +5030,7 @@ function clone(obj) {
   window.PostList = PostList;
 
 }).call(this);
+
 
 
 /* ---- /1MeFqFfFFGQfa1J3gJyYYUvb5Lksczq7nH/js/PostMeta.coffee ---- */
@@ -5900,7 +5901,6 @@ function clone(obj) {
   window.UserList = UserList;
 
 }).call(this);
-
 
 
 /* ---- /1MeFqFfFFGQfa1J3gJyYYUvb5Lksczq7nH/js/ZeroMe.coffee ---- */
